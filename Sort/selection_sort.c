a2c32c273a35881e35c691e15148d60369d8f2da
pythonciot0801y <python.c.iot.2024.08.y@gmail.com>
2024-09-12 13:58:32 +0900

    Update selection_sort.c

--- Sort/selection_sort.c
+++ Sort/selection_sort.c
@@ -1,19 +1,27 @@
-// 選択ソート
-
 #include <stdio.h>
 #include <windows.h>
 #define MAX 10
 #define G_MAX 21
+#define TIMER  750
 
-void printGraph(int* data, int len) {
-
+void printGraph(int* data, int len, int src, int dst) {
+	Sleep(TIMER);
+	printf("\x1b[2J");
+	printf("\x1b[0;0H");
 	for (int y = 0; y < len; y += 1) {
+		if (y == src) {
+			printf("\x1b[31m");
+		}
+		else if (y == dst) {
+			printf("\x1b[36m");
+		}
 		printf(" | ");
 		for (int x = 1; x < G_MAX; x += 1) {
 			if (x <=  *(data + y)) {
 				printf("*");
 			}
 		}
+		printf("\x1b[39m");
 		printf("\n");
 	}
 }
@@ -23,18 +31,18 @@ int main(void) {
 
 	// 選択ソート
 	for (int i = 0; i < MAX; i += 1) {
-		printf("\x1b[2J");
-		printf("\x1b[0;0H");
-		printf("loop counter:%d\n", i);
 		for (int j = i + 1; j < MAX; j += 1) {
-			if (data[i] < data[j]) {
+			printGraph(data, MAX, i, j);
+			if (data[j] < data[i]) {
 				int tmp = data[j];
 				data[j] = data[i];
 				data[i] = tmp;
+				printGraph(data, MAX, j, i);
+			}
+			else {
+				printGraph(data, MAX, i, j);
 			}
 		}
-		printGraph(data, MAX);
-		Sleep(1000);
 	}
 	return 0;
 }
