c4eb4281da147af431808356169fadcb7d58f9c4
decogenasim <decogenasim@gmail.com>
2024-09-24 20:23:24 +0900

    Update Sample1.c

--- Lesson_06/Sample/Sample1.c
+++ Lesson_06/Sample/Sample1.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-/* buy関数の定義 */
-void buy(void)
-{
-   printf("車を買いました。\n");
-}
-
-/* buy関数の呼び出し */
 int main(void)
 {
-   buy();
+   int i;
+
+   for(i=1; i<=5; i++){
+      printf("繰り返しています。\n");
+   }
+
+   printf("繰り返しが終わりました。\n");
 
    return 0;
 }
