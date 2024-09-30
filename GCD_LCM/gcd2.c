3575a863db2c079b7d97d2b00646c03fa72f8d1f
decogenasim <decogenasim@gmail.com>
2024-09-30 21:10:03 +0900

    Update gcd2.c

--- GCD_LCM/gcd2.c
+++ GCD_LCM/gcd2.c
@@ -12,5 +12,5 @@ int gcd(int x, int y){
 int main(void){
     int x = 72, y = 60, z = 42;
     printf("%dと%dの最大公約数は%dです\n", x, y, gcd(x, y));
-    printf("%d，%d，%dの最大公約数は%dです\n", x, y, z, gcd(gcd(x, y), z));
+    printf("%d，%d，%dの最大公約数は%dです\n", x, y, z, gcd(gcd(x, y), z)); // 3つの整数の最大公約数
 }
