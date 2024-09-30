598714bd62d96f23ad392aafb6f86dfe06f434e7
decogenasim <decogenasim@gmail.com>
2024-10-01 08:58:34 +0900

    Update fizzbuzz_02.c

--- FizzBuzz/fizzbuzz_02.c
+++ FizzBuzz/fizzbuzz_02.c
@@ -2,18 +2,18 @@
 
 // 100までのFizzBuzz（15の倍数をチェックしない）
 int main(void){
-  for(int i = 1; i < 101; i += 1){
-    int f3 = i % 3, f5 = i % 5;
-    if(!f3){  // 3で割り切れる
-      printf("Fizz");
+    for(int i = 1; i < 101; i += 1){
+        int f3 = i % 3, f5 = i % 5;
+        if(!f3) {  // 3で割り切れる
+            printf("Fizz");
+        }
+        if(!f5) {  // 5で割り切れる
+            printf("Buzz");
+        }
+        if(f3 && f5) {
+            printf("%d", i);
+        }
+        printf("\n"); // 改行をいれる
     }
-    if(!f5){  // 5で割り切れる
-      printf("Buzz");
-    }
-    if(f3 && f5){
-      printf("%d", i);
-    }
-    printf("\n");
-  }
-  return 0;
+    return 0;
 }
